372e7f9623dc4cb8c2048cce82e49f70ce429872
David Duarte <david.duarte@tecnico.ulisboa.pt>
2015-12-03 15:50:44 +0000

    Fix implementation of HospitalPolicy

--- app/policies/HospitalPolicy.scala
+++ app/policies/HospitalPolicy.scala
@@ -6,19 +6,20 @@ import language.postfixOps
 object HospitalPolicy extends BasicPolicy {
   import stapl.core.dsl._
 
-  subject.role = SimpleAttribute(String)
+  subject.speciality = SimpleAttribute(String)
   subject.treatingPatients = ListAttribute(Number)
   resource.kind = SimpleAttribute(String)
   resource.ownerId = SimpleAttribute(Number)
 
-  val viewPolicies = Policy("Access data policies") := when (subject.id === "physician" & action.id === "read") apply DenyOverrides to (
-    Rule("policy:1") := deny iff !(resource.ownerId in subject.treatingPatients),
-    Rule("policy:2") := deny iff !(subject.role == resource.kind)
+  val readPolicies = Policy("Access data policies") := when (subject.id === "physician" & action.id === "read") apply PermitOverrides to (
+    Rule("policy:1") := permit iff (resource.ownerId in subject.treatingPatients),
+    Rule("policy:2") := permit iff (subject.speciality === resource.kind),
+    Rule("policy:3") := deny
   )
 
   val writePolicies = Policy("Write data policies") := when (subject.id === "physician" & action.id === "write") apply PermitOverrides to (
-    Rule("policy:3") := permit iff (subject.role === resource.kind),
-    Rule("policy:4") := deny
+    Rule("policy:4") := permit iff (subject.speciality === resource.kind),
+    Rule("policy:5") := deny
   )
 
 }
