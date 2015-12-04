053c7d4515d383df8566d43d422724e923e63d28
Ricardo Marques <ricardo.marques@talkdesk.com>
2015-12-04 02:42:07 +0000

    Extend the interaction example

--- app/MRD.scala
+++ app/MRD.scala
@@ -1,18 +1,29 @@
 import java.time.LocalDateTime
 
 import controllers.Application
-import domain.{Database, Cardiology}
+import domain.{Neurology, Database, Cardiology}
 
 
 object MRD extends App {
   val d = Database.addPhysician("Antonio", Cardiology)
   val p = Database.addPatient("Manuel", LocalDateTime.now())
 
-  Application.createRecord(d, p, Cardiology, "Teve um enfarte") match {
+  val recordId = Application.createRecord(d, p, Cardiology, "Teve um enfarte")
+
+  recordId match {
     case Right(id) =>
       println(Application.readRecord(d, p.id, id).merge)
       Application.updateRecord(d, id, "Teve um enfarte bastante severo")
       println(Application.readRecord(d, p.id, id).merge)
     case Left(error) => println(error)
   }
+
+  val e = Database.addPhysician("Merclino", Neurology)
+
+  recordId match {
+    case Right(id) =>
+      println(Application.readRecord(e, p.id, id).merge)
+    case Left(error) => println(error)
+  }
+
 }
